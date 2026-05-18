2385e4db67f3aabcf6b174cd60bb3718a3e72ad9
ringo-1011 <thinh.1011@enotion.io>
2026-05-18 15:03:28 +0700

    should work rn

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -4,12 +4,6 @@ const { ccclass } = _decorator;
 @ccclass('HelloWorld')
 export class HelloWorld extends Component {
     start() {
-
-    }
-
-    WrongNameConvention() {
-        var a = "hello there";
-        console.log("This method name does not follow the camelCase convention."
     }
 }
 
