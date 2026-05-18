7b292452b3d6c59c7e3803916c3e516cf8d98737
ringo-1011 <thinh.1011@enotion.io>
2026-05-18 21:52:02 +0700

    this github action will fail

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -4,6 +4,12 @@ const { ccclass } = _decorator;
 @ccclass('HelloWorld')
 export class HelloWorld extends Component {
     start() {
+        ...
+        //Syntax error
+    }
+
+    InvalidMethodName() {// This method is name is invalid.
+        console.log('This log should be remove before committing.');
     }
 }
 
