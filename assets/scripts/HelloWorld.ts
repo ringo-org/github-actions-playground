30a9c7b7fedcecb71bcb1ea8420519a65e22055c
ringo-1011 <thinh.1011@enotion.io>
2026-05-19 11:15:34 +0700

    remove for test

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -3,10 +3,5 @@ const { ccclass } = _decorator;
 @ccclass('HelloWorld')
 export class HelloWorld extends Component {
 
-    ExampleFunction() { //funciton name should be in camelCase
-        var Message = "Hello World"; //var should be in camelCase
-        console.log(Message); // console log should be remove
-        
-    }
 }
 
