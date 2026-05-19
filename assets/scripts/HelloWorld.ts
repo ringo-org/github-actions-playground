0fae9726af9a02cb8e0f7cd97ce3a8509ff4cf9a
ringo-1011 <thinh.1011@enotion.io>
2026-05-19 14:07:16 +0700

    test type-check

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -4,5 +4,9 @@ const { ccclass } = _decorator;
 @ccclass('HelloWorld')
 export class HelloWorld extends Component {
 
+    start() {
+        console.loggggg('Hello, World!'); //invalid syntax, should be console.log
+    }
+
 }
 
