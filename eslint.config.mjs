8cd4fa2690a4d258d0f79a47b49efc458bb677c8
ringo-1011 <thinh.1011@enotion.io>
2026-05-19 11:14:59 +0700

    udpate lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,26 +4,73 @@ import tsParser from '@typescript-eslint/parser';
 export default [
     {
         files: ['**/*.ts'],
+
         languageOptions: {
             parser: tsParser,
         },
+
         plugins: {
             '@typescript-eslint': tseslint,
         },
+
         rules: {
             '@typescript-eslint/naming-convention': [
                 'error',
+
+                // functions
                 {
                     selector: 'function',
                     format: ['camelCase'],
                 },
+
+                // methods
                 {
                     selector: 'method',
                     format: ['camelCase'],
                 },
+
+                // allow destructuring
+                {
+                    selector: 'variable',
+                    modifiers: ['destructured'],
+                    format: null,
+                },
+
+                // normal variables
+                {
+                    selector: 'variable',
+                    format: ['camelCase'],
+                    leadingUnderscore: 'allow',
+                },
+
+                // const variables
+                {
+                    selector: 'variable',
+                    modifiers: ['const'],
+                    format: [
+                        'UPPER_CASE',
+                    ],
+                    leadingUnderscore: 'allow',
+                },
+
+                // class / interface / type / enum
+                {
+                    selector: 'typeLike',
+                    format: ['PascalCase'],
+                },
             ],
-             'no-console': 'error',
-             '@typescript-eslint/no-unused-vars': 'error',
+
+            // forbid console.log
+            'no-console': 'error',
+
+            // unused variables
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
         },
     },
 ];
