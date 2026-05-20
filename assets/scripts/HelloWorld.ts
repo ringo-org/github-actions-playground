1189add5c9ead1774c71fc5dddaf30cc3ebf601c
ringo-1011 <thinh.1011@enotion.io>
2026-05-20 18:55:24 +0700

    test delete

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -5,13 +5,5 @@ const { ccclass } = _decorator;
 @ccclass('HelloWorld')
 export class HelloWorld extends Component {
 
-    start() {
-        console.log('Đây là một câu tiếng Việt hoàn chỉnh để kiểm tra hệ thống');
-        console.log('xin chao');
-        console.log('tieng viet khong dau');
-        console.log('Xin Chào');
-        console.log('Anh Tai da noi vay thi ok di');
-    }
-
 }
 
