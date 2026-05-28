8d70ec7e5dcd1a0343e0304903b493a637667463
ringo-1011 <thinh.1011@enotion.io>
2026-05-28 15:13:29 +0700

    remove console log

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -5,24 +5,10 @@ const { ccclass } = _decorator;
 export class HelloWorld extends Component {
 
     start() {
-        console.log('Hello World! This is Cocos Creator');
-        console.log('bonjour monde! C\'est Cocos Creator');
-        console.log('こんにちは世界！これはCocos Creatorです');
-        console.log('gacias mundo! Esto es Cocos Creator');
-
-        console.log('Xin chào! Đây là Cocos Creator');
-
-        console.log('Bạn thân ơi vui quá là vui');
-        console.log('bao uoc ma minh van di tim');
-
-        console.log('hu tieu mi, banh mi, com tam, pho');
-
-        console.log('should only linting this line');
         this.dummyFunction();
     }
 
     dummyFunction() {
-        console.log('This is a dummy function');
     }
 }
 
