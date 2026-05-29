50aad5430fc936f4b9c841a5e31f779fb41ce2fd
ringo-1011 <thinh.1011@enotion.io>
2026-05-29 09:51:25 +0700

    udpate

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -5,10 +5,10 @@ const { ccclass } = _decorator;
 export class HelloWorld extends Component {
 
     start() {
-        
+        this.newFunction();
     }
 
     newFunction(){
-        
+        console.log("this is a log");
     }
 }
