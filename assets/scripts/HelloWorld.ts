1932e386a1c68b786be5954cc989639c7d97dad5
ringo-1011 <thinh.1011@enotion.io>
2026-05-29 15:21:09 +0700

    update

--- assets/scripts/HelloWorld.ts
+++ assets/scripts/HelloWorld.ts
@@ -9,6 +9,6 @@ export class HelloWorld extends Component {
     }
 
     newFunction(){
-        var a = "day la chuoi tieng viet";
+        var a = "day la chuoi tieng viet, mot tram phan tram";
     }
 }
